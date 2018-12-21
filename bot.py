9fba83d05b75fc19d66f3343f0714f5498905a4a
Fishbuilder <jeroen.vds.99@gmail.com>
2018-12-21 21:14:23 +0100

    added connect4 cog

--- bot.py
+++ bot.py
@@ -1,58 +1,67 @@
 ﻿import discord
 from discord.ext import commands
+#import checks to do: move all checks to 1 python file
 
 with open('D:\\Python\\Discord\\token.txt', 'r') as infile:
     data = [i.strip('\n') for i in infile.readlines()]
     TOKEN = data[0] #first line in token.txt
-    #OWNER = data[1] #second line in token.txt
+    OWNER = data[1] #second line in token.txt
 
 client = commands.Bot(command_prefix = '!') #not used
 emos = {}
 
+def is_owner(ctx):
+    return ctx.message.author.id == OWNER
+
 @client.event
 async def on_ready():
     print('Bot is ready')
-
     #gets all custom emojis
     for x in client.get_all_emojis():
         emos[x.name] = x
     print(emos)
-        
-@client.event
-async def on_message(message):
-    content = message.content.lower()
-    if 'router' in content or 'splitter' in content:
-        await client.add_reaction(message, emos['router'])
-    await client.process_commands(message)
-
-@client.event
-async def on_message_edit(before, after):
-    await on_message(after)
-   
-@client.command(pass_context=True,
-                brief='Deletes messages')
-@commands.has_role('test')
-async def clear(ctx, limit=99):
-    channel = ctx.message.channel
-    print(channel.name)
-    if 1:#str(ctx.message.author).split('#')[-1] == OWNER.split('#')[-1]:
-        message = [msg async for msg in client.logs_from(channel, limit=int(limit)+1)]
-        print(len(message))
-        await client.delete_messages(message)
-
-@client.command(pass_context=True,
-                brief='connect4')
-async def image(ctx, file):
-    await client.send_file(ctx.message.channel, file)
-    try:
-        embed = discord.Embed(title='dit is een test')
-        await client.send_message(ctx.message.channel, embed=embed)
-    except Exception as e:
-        print(e)
-@client.command(pass_context=True)
-async def get(ctx):
-    for i in client.servers:
-        await client.say(i.id)
     
+#loading cogs
+@client.command(pass_context=True)
+@commands.check(is_owner)
+async def load(ctx, cog=None):
+    if not cog:
+        await client.say('add cog')
+        return
+    try:
+        client.load_extension(f'cogs.{cog}')
+        print(f'loaded {cog}')
+    except Exception as e:
+        print(f'failed to load {cog} reason: [{e}]')
 
+@client.command(pass_context=True)
+@commands.check(is_owner)
+async def unload(ctx, cog=None):
+    if not cog:
+        await client.say('add cog')
+        return
+    try:
+        client.unload_extension(f'cogs.{cog}')
+        print(f'unloaded {cog}')
+    except Exception as e:
+        print(f'failed to unload {cog} reason: [{e}]')
+
+#error handling
+@client.event
+async def on_error(error):
+    print(error)
+
+'''
+@client.event
+async def on_command_error(ctx,error):
+    print(type(error))
+'''
+
+#testing
+@client.command(pass_context=True)
+@commands.check(is_owner)
+async def eval(ctx, *args):
+    print(eval(''.join((i+' ' for i in args))))
+    
+    
 client.run(TOKEN)
