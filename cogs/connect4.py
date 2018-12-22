f2442a8213fae4236ef2d5ebb84ac1d6ca3bad1e
Fishbuilder <jeroen.vds.99@gmail.com>
2018-12-22 11:56:39 +0100

    hotfix

--- cogs/connect4.py
+++ cogs/connect4.py
@@ -3,41 +3,38 @@ from discord.ext import commands
 #import checks
 
 def is_owner(ctx):
-    return ctx.message.author.id == OWNER
+    return ctx.message.author.id != 5
 
 class Connect4(object):
     def __init__(self, bot):
         self.bot = bot
-        self.join = {}
+        self.games = {}
         self.mode = 0 #0:host, 1:join
 
     @commands.group(pass_context=True)
     async def connect4(self, ctx):
         if ctx.invoked_subcommand is None:
             await self.bot.say('You will need to add a subcommand ex: connect4 host')
-
+        return ctx
+    
     @connect4.command(pass_context=True)
-    @commands.check(is_owner)
     async def change_mode(self, ctx):
         self.mode = (self.mode+1)%2
         print('join' if self.mode else 'host')
+        return
 
-    @connect4.command()
-    async def join(self):
-        print('join')
-        print(ctx.message.author.name)
-        if not self.mode:
-            await self.bot.say('*DISABLED* use host instead')
-            return
-        pass
+    @connect4.command(pass_context=True)
+    async def join(self, ctx):
+        print(ctx.message.author)
+        
 
-    @connect4.command()
-    async def host(self):
+    @connect4.command(pass_context=True)
+    async def host(self, ctx):
+        print(ctx.message.author)
         if self.mode:
             await self.bot.say('*DISABLED* use join instead')
             return
         
-    
 
 #load cog
 def setup(bot):
